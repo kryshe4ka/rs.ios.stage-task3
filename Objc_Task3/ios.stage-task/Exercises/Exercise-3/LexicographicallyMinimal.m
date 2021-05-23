edc245e6e52ab2bd2a31294f73f6531ac9feb7f6
kryshe4ka <kryshe4ka@gmail.com>
2021-05-23 23:28:46 +0300

    feat: add LexicographicallyMinimal implementation

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,33 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    
+    NSMutableString *str1 = [NSMutableString stringWithString:string1];
+    NSMutableString *str2 = [NSMutableString stringWithString:string2];
+    NSMutableString *resultStr = [NSMutableString stringWithString:@""];
+    NSRange range = NSMakeRange(0, 1);
+    
+    
+    while (!(str1.length == 0) && !(str2.length == 0)) {
+        
+        NSComparisonResult compareResult = [str1 compare:str2];
+        NSLog(@"%li", (long)compareResult);
+        
+        if (compareResult == 1) {
+            [resultStr appendString:[str2 substringWithRange:range]];
+            [str2 deleteCharactersInRange:range];
+        } else {
+            [resultStr appendString:[str1 substringWithRange:range]];
+            [str1 deleteCharactersInRange:range];
+        }
+        NSLog(@"%@", resultStr);
+    }
+    [resultStr appendString:str1];
+    [resultStr appendString:str2];
+    
+    NSString *resultString = [NSString stringWithString:resultStr];
+    
+    return resultString;
 }
 
 @end
