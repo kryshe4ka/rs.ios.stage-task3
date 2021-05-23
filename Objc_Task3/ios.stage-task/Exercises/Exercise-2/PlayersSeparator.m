e2350986ac7cbe1d70ab208989fa670f8246e957
kryshe4ka <kryshe4ka@gmail.com>
2021-05-23 19:34:47 +0300

    feat: add PlayersSeparator implementation

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,25 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    int count = 0;
+    
+    if (!ratingArray || (ratingArray.count < 3)) {
+        return count;
+    }
+    
+    NSInteger n = ratingArray.count;
+    
+    for (int i = 0; i < n-2; i++) {
+        for (int j = i+1; j < n; j++) {
+            for (int k = j+1; k < n; k++) {
+                if ((ratingArray[i].intValue < ratingArray[j].intValue && ratingArray[j].intValue < ratingArray[k].intValue)
+                    || (ratingArray[i].intValue > ratingArray[j].intValue && ratingArray[j].intValue > ratingArray[k].intValue)) {
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
 }
 
 @end
